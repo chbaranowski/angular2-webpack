8f297e209c2868ea016138f93f7dca72fbc3eb39
Christian Baranowski <develop@pro-pix.de>
2016-10-24 20:40:55 +0200

    Angular 2.0 playground - Some small changes to play around with the project.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,12 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  options: HighchartsOptions;
+
   constructor() {
     // Do stuff
+    console.log('init home component');
+
+    this.options = {
+      title: { text: 'simple chart' },
+      series: [{
+        data: [29.9, 71.5, 106.4, 129.2],
+      }]
+    };
+
   }
 
   ngOnInit() {
     console.log('Hello Home');
+  }
+
+  clickAction() {
+    console.log('click action implmeneted...');
   }
 
 }
